a196064b13f26ddf6b8076546352c0671916f7d4
Siddhesh Gore <sidh711@hotmail.com>
2017-09-05 15:35:40 +0530

    Initial commit with dummy records.

---
+++ crud/filters.py
@@ -0,0 +1,22 @@
+""" Filter list for curd model
+  QuestionFilter
+"""
+import django_filters
+
+from . import models
+
+class QuestionFilter(django_filters.FilterSet):
+    """Search and retrive data from Question records
+    """
+    class Meta:
+        model = models.Question
+        char_search_fields = [
+            'exact',
+            'contains',
+            'in',
+            'startswith',
+            'icontains',
+        ]
+        fields = {
+            "title": char_search_fields
+        }
